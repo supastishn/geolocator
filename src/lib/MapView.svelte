776dfddc92e7f063d8752fc75ce7322093edac1c
supastishn <omareeto2012@hotmail.com>
2025-06-02 08:24:30 +0000

    feat: Integrate Leaflet.js with OpenStreetMap

--- src/lib/MapView.svelte
+++ src/lib/MapView.svelte
@@ -1,46 +1,46 @@
 <script>
   import { onMount } from 'svelte';
-  import { Loader } from '@googlemaps/js-api-loader';
-  import { settings } from './stores.js';
-  
+
   export let lat;
   export let lng;
-  
+
   let mapContainer;
   let map;
-  
+
   onMount(async () => {
-    const { mapsKey } = $settings;
-    
-    const loader = new Loader({
-      apiKey: mapsKey,
-      version: "weekly"
+    // Dynamically import Leaflet only on client
+    const L = await import('leaflet');
+    // Fix for default icon path in many bundlers
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
     });
-    
-    const google = await loader.load();
-    
-    map = new google.maps.Map(mapContainer, {
-      center: { lat: Number(lat), lng: Number(lng) },
-      zoom: 14,
-      mapTypeId: 'satellite'
-    });
-    
-    new google.maps.Marker({
-      position: { lat: Number(lat), lng: Number(lng) },
-      map,
-      title: "Identified Location"
-    });
+
+    map = L.map(mapContainer).setView([Number(lat), Number(lng)], 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([Number(lat), Number(lng)]).addTo(map)
+      .bindPopup('Identified Location')
+      .openPopup();
   });
 </script>
 
 <div bind:this={mapContainer} class="map-container" />
 
 <style>
+  @import "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";
+
   .map-container {
     height: 400px;
     width: 100%;
     margin-top: 1rem;
     border: 1px solid #ccc;
     border-radius: 4px;
+    z-index: 0;
   }
 </style>
